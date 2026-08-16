ad96136f94e6a0cb9ef2f54dd85f0bf436e444fe
James Woolfenden <james.woolfenden@gmail.com>
2026-08-16 14:53:58 +0100

    holden refreshed

--- google_document_ai_processor.tf
+++ google_document_ai_processor.tf
@@ -3,4 +3,6 @@ resource "google_document_ai_processor" "main" {
   display_name = var.display_name
   type         = var.type
   kms_key_name = var.kms_key_name
+
+  depends_on = [google_kms_crypto_key_iam_member.documentai_service_agent]
 }
